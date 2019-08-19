638462909c4abf097b232f40bbb79dbabc662811
Sopheak088 <ourlesson@gmail.com>
2019-08-19 22:17:39 +0700

    print text

--- JavaRUPP/src/javarupp/JavaRUPP.java
+++ JavaRUPP/src/javarupp/JavaRUPP.java
@@ -1,21 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package javarupp;
 
-/**
- *
- * @author Darling-PC
- */
 public class JavaRUPP {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Royal University of Phnom Penh");
+        System.out.println("Name : Vann Davin");
+        System.out.println("Gender : Female\nClass : A2");
+        System.out.println("Thank You!");
     }
-    
 }
