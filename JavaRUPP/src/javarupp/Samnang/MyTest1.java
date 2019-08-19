232cb65fb50b26243b77babe43a845b0d5974b6f
Sopheak088 <ourlesson@gmail.com>
2019-08-19 22:42:40 +0700

    Commit package

--- JavaRUPP/src/javarupp/Samnang/MyTest1.java
+++ JavaRUPP/src/javarupp/Samnang/MyTest1.java
@@ -10,5 +10,9 @@ package javarupp.Samnang;
  * @author Darling-PC
  */
 public class MyTest1 {
-    
+    public static void main(String[] args) {
+        System.out.println("Royal University of Phnom Penh");
+        System.out.println("Name : Samnang");
+        System.out.println("Thank You!");
+    }
 }
