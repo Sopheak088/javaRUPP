7f31b7175b84b3557d12f272aa37dad1eddc6975
Sopheak088 <ourlesson031@gmail.com>
2019-08-19 22:35:38 +0700

    vin vin commit 1

--- JavaRUPP/src/javarupp/Davin/Test1.java
+++ JavaRUPP/src/javarupp/Davin/Test1.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package javarupp.Davin;
 
-/**
- *
- * @author Darling-PC
- */
+package javarupp.Davin;
 public class Test1 {
-    
+    public static void main(String[] args) {
+        System.out.println("Royal University of Phnom Penh");
+        System.out.println("Name : Vann Davin");
+        System.out.println("Gender : Female\nClass : A2");
+        System.out.println("Thank You!");
+    }
 }
